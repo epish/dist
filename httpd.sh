7aec4b420d66b66e73f020d5468f9287e3c57731
epish <epishan@gmail.com>
2014-02-24 17:58:37 +0400

    changed httpd.sh init.d script

--- httpd.sh
+++ httpd.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Startup script for the Apache Web Server
 #
-# chkconfig: 345 85 15
-# description: Apache is a World Wide Web server.&nbsp; It is used to serve \
-#               HTML files and CGI.
+# chkconfig: - 85 15
+# description: Apache is a World Wide Web server.  It is used to serve \
+#              HTML files and CGI.
 # processname: httpd
 # pidfile: /usr/local/apache2/logs/httpd.pid
 # config: /usr/local/apache2/conf/httpd.conf
@@ -12,36 +12,81 @@
 # Source function library.
 . /etc/rc.d/init.d/functions
 
+if [ -f /etc/sysconfig/httpd ]; then
+        . /etc/sysconfig/httpd
+fi
+
+# This will prevent initlog from swallowing up a pass-phrase prompt if
+# mod_ssl needs a pass-phrase from the user.
+INITLOG_ARGS=""
+
+# Path to the apachectl script, server binary, and short-form for messages.
+apachectl=/usr/local/apache2/bin/apachectl
+httpd=/usr/local/apache2/bin/httpd
+pid=$httpd/logs/httpd.pid
+prog=httpd
+RETVAL=0
+
+
+# The semantics of these two functions differ from the way apachectl does
+# things -- attempting to start while running is a failure, and shutdown
+# when not running is also a failure.  So we just do it the way init scripts
+# are expected to behave here.
+start() {
+        echo -n $"Starting $prog: "
+        daemon $httpd $OPTIONS
+        RETVAL=$?
+        echo
+        [ $RETVAL = 0 ] && touch /var/lock/subsys/httpd
+        return $RETVAL
+}
+stop() {
+        echo -n $"Stopping $prog: "
+        killproc $httpd
+        RETVAL=$?
+        echo
+        [ $RETVAL = 0 ] && rm -f /var/lock/subsys/httpd $pid
+}
+reload() {
+        echo -n $"Reloading $prog: "
+        killproc $httpd -HUP
+        RETVAL=$?
+        echo
+}
+
 # See how we were called.
 case "$1" in
-    start)
-        echo -n "Starting httpd: "
-        daemon /usr/local/apache2/bin/httpd -DSSL
-        echo
-        touch /var/lock/subsys/httpd
+  start)
+        start
         ;;
-    stop)
-        echo -n "Shutting down http: "
-        killproc httpd
-        echo
-        rm -f /var/lock/subsys/httpd
-        rm -f /usr/local/apache2/logs/httpd.pid
+  stop)
+        stop
         ;;
-    status)
-        status httpd
+  status)
+        status $httpd
+        RETVAL=$?
         ;;
-    restart)
-        $0 stop
-        $0 start
+  restart)
+        stop
+        start
         ;;
-    reload)
-        echo -n "Reloading httpd: "
-        killproc httpd -HUP
-        echo
+  condrestart)
+        if [ -f $pid ] ; then
+                stop
+                start
+        fi
         ;;
-    *)
-        echo "Usage: $0 {start|stop|restart|reload|status}"
+  reload)
+        reload
+        ;;
+  graceful|help|configtest|fullstatus)
+        $apachectl $@
+        RETVAL=$?
+        ;;
+  *)
+        echo $"Usage: $prog {start|stop|restart|condrestart|reload|status"
+		echo $"|fullstatus|graceful|help|configtest}"
         exit 1
 esac
 
-exit 0
+exit $RETVAL
